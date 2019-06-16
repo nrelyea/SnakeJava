1a32a28963f595cdd5a36dba381da56ea9628c07
OsoGP <40732784+OsoGP@users.noreply.github.com>
2019-06-16 14:38:05 -0700

    Starting Challenge

--- Snake.java
+++ Snake.java
@@ -7,34 +7,28 @@ import java.awt.event.KeyListener;
 
 public class Snake extends Applet implements KeyListener{
 	
-	private Rectangle rect;
-	
 	public void init() {
-		this.addKeyListener(this);
-		
-		rect = new Rectangle(0,0,50,50);
+		this.addKeyListener(this);		
 	}
 	
 	public void paint(Graphics g) {
 		setSize(500, 500);
 		
 		Graphics2D g2d = (Graphics2D)g;
-		g2d.fill(rect);
+		g2d.fill(new Rectangle(0,0,50,50));
+		g2d.fill(new Rectangle(450,450,50,50));
 	}
 
-	@Override
 	public void keyPressed(KeyEvent arg0) {
 		System.out.println("YEET");
 		
 	}
 
-	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
