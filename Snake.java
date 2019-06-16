1fc2a8f37de093f25e1f4ce5464c8e35f64b72a2
OsoGP <40732784+OsoGP@users.noreply.github.com>
2019-06-16 13:28:06 -0700

    Simplification - Applet

--- Snake.java
+++ Snake.java
@@ -1,12 +1,43 @@
-import javax.swing.JFrame;
+import java.applet.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Snake {
-	public static void main(String[] args) {
-		System.out.println("running");
-		GUI gui = new GUI();
-		gui.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setSize(300,200);
-		gui.setVisible(true);
+public class Snake extends Applet implements KeyListener{
+	
+	private Rectangle rect;
+	
+	public void init() {
+		this.addKeyListener(this);
+		
+		rect = new Rectangle(0,0,50,50);
 	}
+	
+	public void paint(Graphics g) {
+		setSize(500, 500);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.fill(rect);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		System.out.println("YEET");
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
