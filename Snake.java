cddc3b3c68a6e428f03f19f2d32330844a1766f2
OsoGP <40732784+OsoGP@users.noreply.github.com>
2019-06-16 18:35:24 -0700

    Arrow keys working

--- Snake.java
+++ Snake.java
@@ -1,35 +1,69 @@
 import java.applet.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Snake extends Applet implements KeyListener{
 	
+	Point headPoint;
+	
+	
 	public void init() {
-		this.addKeyListener(this);		
+		//Thread t = new Thread(this);
+		
+		
+		
+		
+		this.addKeyListener(this);
+				
+		headPoint = new Point(0,0);		
+	}
+	
+	public void run() {
+		System.out.println("yeet");
 	}
 	
 	public void paint(Graphics g) {
 		setSize(500, 500);
 		
 		Graphics2D g2d = (Graphics2D)g;
-		g2d.fill(new Rectangle(0,0,50,50));
-		g2d.fill(new Rectangle(450,450,50,50));
-	}
-
-	public void keyPressed(KeyEvent arg0) {
-		System.out.println("YEET");
+		g2d.fill(new Rectangle(headPoint.x,headPoint.y,50,50));
 		
 	}
 
-	public void keyReleased(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_RIGHT:
+			if(headPoint.x < 450) {
+				headPoint = new Point(headPoint.x + 50, headPoint.y);
+			}
+			repaint();
+			break;		
+		case KeyEvent.VK_LEFT:
+			if(headPoint.x > 0) {
+				headPoint = new Point(headPoint.x - 50, headPoint.y);
+			}
+			repaint();
+			break;
+		case KeyEvent.VK_UP:
+			System.out.println("UP");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("DOWN");
+			break;
+		}
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
