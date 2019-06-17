6a892458fe753f87d1d915b6e5267999a4552c0a
OsoGP <40732784+OsoGP@users.noreply.github.com>
2019-06-16 19:35:44 -0700

    Stream 1 Leftoff point

--- Snake.java
+++ Snake.java
@@ -1,37 +1,80 @@
 import java.applet.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Snake extends Applet implements KeyListener{
 	
+	int width;
+	int height;
+	int squareSize;
+	
+	boolean gameOver;
+	
 	Point headPoint;
 	
+	Graphics bufferGraphics;
+	Image offscreen;
 	
-	public void init() {
-		//Thread t = new Thread(this);
+	long currentTime;
+	
+	
+	public Snake() {
 		
+		width = 500;
+		height = 500;
+		squareSize = 50;
 		
+		gameOver = false;
 		
+		headPoint = new Point(0,0);	
+		
+		offscreen = createImage(width,height);
+		bufferGraphics = offscreen.getGraphics();
 		
 		this.addKeyListener(this);
 				
-		headPoint = new Point(0,0);		
+			
 	}
 	
-	public void run() {
-		System.out.println("yeet");
+	public void start() {
+		currentTime = System.currentTimeMillis();
+		
+//		Timer time = new Timer(1000, this);
+//		time.start();
+////		while(!gameOver) {
+////			
+////		}
+//		
+//		time.stop();
+//		currentTime = System.currentTimeMillis() - currentTime;
+//		repaint();
+		
+		
+		Timer timer = new Timer();
+		timer.schedule(new SayHello(), 0, 5000);
 	}
 	
 	public void paint(Graphics g) {
-		setSize(500, 500);
+		bufferGraphics.clearRect(0,0,width,height);
+		bufferGraphics.fillRect(headPoint.x,headPoint.y,squareSize,squareSize);
 		
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.fill(new Rectangle(headPoint.x,headPoint.y,50,50));
+		g.drawImage(offscreen, 0, 0, this);
+		Toolkit.getDefaultToolkit();
 		
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -68,4 +111,13 @@ public class Snake extends Applet implements KeyListener{
 		
 	}
 	
+	
+	
 }
+
+class SayHello extends TimerTask {
+    public void run() {
+       System.out.println("Hello World!"); 
+    }
+}
+
